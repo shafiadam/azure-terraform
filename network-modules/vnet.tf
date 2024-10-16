8f9df2bdaf6ec42de9945f64f5cb0d90762ac2ac
Usamah Shafi <54491266+usamahshafi@users.noreply.github.com>
2024-10-16 15:20:43 -0700

    Create vnet.tf

---
+++ network-modules/vnet.tf
@@ -0,0 +1,8 @@
+resource "azurerm_virtual_network" "landing_vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
