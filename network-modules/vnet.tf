ab981c864107227d67a39ac5d5f543e8998ecb16
Usamah Shafi <54491266+usamahshafi@users.noreply.github.com>
2024-10-16 15:25:12 -0700

    Delete network-modules directory

--- network-modules/vnet.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_virtual_network" "landing_vnet" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  tags = var.tags
-}
